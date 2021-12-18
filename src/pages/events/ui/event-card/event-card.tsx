9c25d3cb52eb9ceadfdeeb24ca672d9cdc188212
xalvaine <xalvaine@gmail.com>
2021-12-18 15:23:46 +0300

    [feat] Improve events

--- src/pages/events/ui/event-card/event-card.tsx
+++ src/pages/events/ui/event-card/event-card.tsx
@@ -1,5 +1,22 @@
-import { Card } from 'antd'
+import { Card, Typography } from 'antd'
+import styles from './event-card.module.scss'
 
 export const EventCard = () => {
-  return <Card>asdsad</Card>
+  return (
+    <Card
+      cover={
+        <img
+          alt='example'
+          className={styles.image}
+          src='https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png'
+        />
+      }
+    >
+      <Typography.Title level={5}>Хакатон от ICT</Typography.Title>
+      <Typography.Paragraph>
+        Научно-исследовательская работа
+      </Typography.Paragraph>
+      <Typography.Text disabled>Можно получить 5 баллов</Typography.Text>
+    </Card>
+  )
 }
--- src/pages/events/ui/events/events.tsx
+++ src/pages/events/ui/events/events.tsx
@@ -1,11 +1,19 @@
 import { PageTitle } from 'shared/ui'
 import { EventCard } from '..'
 import styles from './events.module.scss'
+import { Tabs } from 'antd'
 
 export const Events = () => {
   return (
     <>
-      <PageTitle title='Мероприятия' />
+      <Tabs className={styles.tabs} defaultActiveKey='1'>
+        <Tabs.TabPane tab='Учебные' key='1' />
+        <Tabs.TabPane tab='Научно-исследовательские' key='2' />
+        <Tabs.TabPane tab='Общественные' key='3' />
+        <Tabs.TabPane tab='Культурно-творческие' key='4' />
+        <Tabs.TabPane tab='Спортивные' key='5' />
+      </Tabs>
+      <PageTitle title='Учебные мероприятия' />
       <div className={styles.wrapper}>
         <EventCard />
         <EventCard />
